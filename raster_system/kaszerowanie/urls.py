34b99899f908f6015a640e4e4c9d55bea699eb3c
Konrad Siwak <8471540-konrad.siwak@users.noreply.gitlab.com>
2022-08-21 21:10:55 +0200

    KASZEROWANIE - Zmiana sposobu pozyskiwania danych
    rozdzielono pobieranie danych, osobno gotowe osobno do zrobienia
    na liście do zrobienia wyróżniono rzeczy zrobione

--- raster_system/kaszerowanie/urls.py
+++ raster_system/kaszerowanie/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import KaszerowanieDetailView, KaszerowanieListView
+from .views import KaszerowanieDetailView, KaszerowanieListView, kasz_list
 
 app_name = 'kaszerowanie'
 
 urlpatterns = [
-    path('', KaszerowanieListView.as_view(), name='kasz_list'),
+    # path('', KaszerowanieListView.as_view(), name='kasz_list'),
+    path('', kasz_list , name='kasz_list'),
     path('kasz/<pk>/', KaszerowanieDetailView.as_view(), name='kasz_detail'),
 ]
--- raster_system/kaszerowanie/views.py
+++ raster_system/kaszerowanie/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404, HttpResponse
 from django.views.generic import DetailView, ListView
 
 # Create your views here.
 from .models import Kaszerowanie
+
+def kasz_list(request):
+    kasz_zrobic = Kaszerowanie.objects.filter(kasz_gotowe="False")
+    kasz_zrobione = Kaszerowanie.objects.filter(kasz_gotowe="True")
+    return render(request,"kasz_list_view.html", {"kasz_zrobic":kasz_zrobic, "kasz_zrobione":kasz_zrobione} )
 
 
 class KaszerowanieListView(ListView):
