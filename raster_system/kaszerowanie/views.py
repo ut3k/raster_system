494b4e419fe2edfc7a5f5281d89f9f4fb6b8c823
Konrad Siwak <8471540-konrad.siwak@users.noreply.gitlab.com>
2023-01-31 19:59:03 +0100

    KASZEROWANIE- widok szczegółowy

--- raster_system/kaszerowanie/views.py
+++ raster_system/kaszerowanie/views.py
@@ -31,7 +31,7 @@ class KaszerowanieListView(ListView):
 
 class KaszerowanieDetailView (DetailView):
     model = Kaszerowanie
-    template_name = "kasz_list_view.html"
+    template_name = "kasz_detail_view.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
