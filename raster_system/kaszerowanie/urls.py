e5a103218612b096d9a8e12d8a40acf1d93834de
Konrad Siwak <8471540-konrad.siwak@users.noreply.gitlab.com>
2023-01-31 15:07:49 +0100

    KASZEROWANIE - tabele widoków list

--- raster_system/kaszerowanie/urls.py
+++ raster_system/kaszerowanie/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import KaszerowanieDetailView, KaszerowanieListView, kasz_list
+from .views import (KaszerowanieDetailView,
+                    KaszerowanieListView,
+                    kasz_list,
+                    kasz_list_done,
+                    kasz_list_all,)
 
 app_name = 'kaszerowanie'
 
 urlpatterns = [
     # path('', KaszerowanieListView.as_view(), name='kasz_list'),
-    path('', kasz_list , name='kasz_list'),
+    path('todo', kasz_list , name='kasz_list'),
+    path('all', kasz_list_all , name='kasz_list_all'),
+    path('done', kasz_list_done , name='kasz_list_done'),
     path('kasz/<pk>/', KaszerowanieDetailView.as_view(), name='kasz_detail'),
 ]
