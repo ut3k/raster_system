282ec99037ee9d8654ba9b3f7481275887c9f3f9
Konrad Siwak <8471540-konrad.siwak@users.noreply.gitlab.com>
2023-04-20 10:21:01 +0200

    mały refator codu, tabela ma teraz domyślnie bg-opacity

--- raster_system/static/js/app.js
+++ raster_system/static/js/app.js
@@ -5,12 +5,10 @@ function rename_status(list){
     if (list[i].innerHTML=="False") {
       list[i].innerHTML="NIE";
       list[i].parentElement.classList.add("bg-danger");
-      list[i].parentElement.classList.add("bg-opacity-25");
     }
     else {
       list[i].innerHTML="TAK"
       list[i].parentElement.classList.add("bg-success");
-      list[i].parentElement.classList.add("bg-opacity-25");
     }
   }
 }
