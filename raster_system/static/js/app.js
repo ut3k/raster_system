a749061d7734210cbeab9c62b34bd7deb3ec87e3
Konrad Siwak <8471540-konrad.siwak@users.noreply.gitlab.com>
2023-04-20 09:16:06 +0200

    javascript - kolorowanie rzędu w statusie tabeli

--- raster_system/static/js/app.js
+++ raster_system/static/js/app.js
@@ -1,14 +1,18 @@
 const job_status_list = document.getElementsByClassName('jobstatus');
 
 function rename_status(list){
-    for (let i = 0; i < list.length; i++) {
-        if (list[i].innerHTML=="False") {
-            list[i].innerHTML="NIE";
-          }
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].innerHTML=="False") {
+      list[i].innerHTML="NIE";
+      list[i].parentElement.classList.add("bg-danger");
+      list[i].parentElement.classList.add("bg-opacity-25");
+    }
     else {
       list[i].innerHTML="TAK"
+      list[i].parentElement.classList.add("bg-success");
+      list[i].parentElement.classList.add("bg-opacity-25");
     }
-      }
+  }
 }
 
 rename_status(job_status_list)
